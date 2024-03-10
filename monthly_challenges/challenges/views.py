70d4fe31363f3166413729f03d178fa47a2c57a6
bmmukhridin <bmmukhridin@icloud.com>
2024-03-09 23:52:34 -0800

    added 404 page

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+# from django.template.loader import render_to_string
 
 
 monthly_challenge = {
@@ -47,4 +48,6 @@ def monthly_challenges(request, month):
             "month_name": month.capitalize()
         })
     except:
-        return HttpResponseNotFound("This month not supported")
+        raise Http404("404.html")
+        # data_resposne = render_to_string("404.html")
+        # return HttpResponseNotFound(data_resposne)
